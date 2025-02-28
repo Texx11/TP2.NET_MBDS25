58b8999d1bf269aa008e5e92d6d1a7df02e03b16
vivien.lugagne <vivien.lugagne@etu.estia.fr>
2025-02-28 01:05:16 +0100

    Correction bug création categorie

--- Gauniv.WebServer/Models/CreateViewModel.cs
+++ Gauniv.WebServer/Models/CreateViewModel.cs
@@ -63,7 +63,7 @@ namespace Gauniv.WebServer.Models
         public int Id { get; set; }
         public String? Name { get; set; }
 
-        public required List<Category> Categories {get; set;}
+        public List<Category> Categories { get; set; } = new List<Category>();
     }
 
 
